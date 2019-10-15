613f5f6c0c5a1125a891bb9dacd830fcf44f42e6
Unknown <sergioa@mail.com>
2019-10-15 13:26:34 -0300

    Calendario

--- application/modules/frontend/views/pages/index_carrera.php
+++ application/modules/frontend/views/pages/index_carrera.php
@@ -1,28 +1,3 @@
-
-<!-- <html lang="es">
-	<body>	
-		<div class="container">
-			<div class="row col-12 align-content-center" >
-				<?php foreach ($carreras as $row) {?>
-					<a href="<?= base_url("/carrera/".$row->id) ?>">
-						<div class="card" style="width: 20rem; border-radius: 20px;">
-							<?php if ($row->imagen <> ''){ ?>
-								<img class="card-img-top" style="border-radius: 20px;" src="<?= base_url(IMAGES_UPLOAD.$row->imagen) ?>" alt="<?= $row->nombre?>">
-							<?php }else { ?>
-								<img class="card-img-top" src="<?= base_url('assets/images/default.jpg') ?>" alt="<?= $row->nombre?>">
-							<?php } ?>
-							<div class="card-body">
-								<a href="<?= base_url("/carrera/".$row->id) ?>" class="btn btn-primary float-right">Ver Carrera</a>
-							</div>
-						</div>
-					</a>
-				<?php } ?> 
-			</div>
-		</div>
-	</body>
-	<hr>
-</html> -->
-
 <div class="container-fluid text-center">    
   	<div class="row content">
     
@@ -30,19 +5,9 @@
 	    <div class="container col-lg-2 sidenav eventos" id="tourpackages-carousel">
         	<div class="row">
 	        	<div class="thumbnail">
-	             	<div class="caption">
-		                <div class='col-lg-12'>
-		                    <span class="glyphicon glyphicon-credit-card"></span>
-		                    <span class="glyphicon glyphicon-trash pull-right text-primary"></span>
-		                </div>
-		                <div class='col-lg-12 well well-add-card'>
-		                    <h4>Publicacion de articulo actualizada</h4>
-		                </div>
-		                <div class='col-lg-12'>
-		                    <p>kjhkj/p>
-		                    <p class"text-muted">Fecha: 12-08</p>
-		                </div>
-	            	</div>
+	             	<div class="container" style="min-height: 250px;">
+		                <?php echo $calendario ?>
+		            </div>
 	        	</div>
 	        	
 
